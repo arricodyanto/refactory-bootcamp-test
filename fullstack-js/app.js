1bbf5fcec0b733fad3917539db01d2b6c482d857
arrricodyanto <arricohandyanto@gmail.com>
2021-10-27 13:21:48 +0700

    add form login

---
+++ fullstack-js/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+    res.render('login', {
+        layout: 'layouts/main-layout',
+        title: 'Login Page - Alakadarnya'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
